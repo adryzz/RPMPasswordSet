7bfde125db8d22ac13566c5394e5edde2e9353c4
Adryzz <46694241+adryzz@users.noreply.github.com>
2020-04-19 15:37:50 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace RPMPasswordSet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Old password (if there is one saved in this pc)";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(12, 29);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.PasswordChar = '*';
+            this.maskedTextBox1.Size = new System.Drawing.Size(222, 20);
+            this.maskedTextBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "New password";
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(12, 72);
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.PasswordChar = '*';
+            this.maskedTextBox2.Size = new System.Drawing.Size(222, 20);
+            this.maskedTextBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Repeat new password";
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Location = new System.Drawing.Point(12, 111);
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.PasswordChar = '*';
+            this.maskedTextBox3.Size = new System.Drawing.Size(222, 20);
+            this.maskedTextBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 41);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Set";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Wiew this project on ";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(110, 186);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "GitHub";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 243);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "RPMPasswordSet";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using Microsoft.Win32;
+
+namespace RPMPasswordSet
+{
+    public partial class Form1 : Form
+    {
+        string Key = @"HKEY_CURRENT_USER\Software\RemotePresentationManager\Passwords";
+        string Value = @"Key";
+        string Current = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string current = null;
+            try
+            {
+                current = Registry.GetValue(Key, Value, null).ToString();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(this, ex.Message, "RPMPasswordSet", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //Application.Exit();
+            }
+            if (current == null)
+            {
+                label1.Enabled = false;
+                maskedTextBox1.Enabled = false;
+            }
+            else
+            {
+                Current = current;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SHA512 sha = new SHA512Managed();
+                if (Current == null)
+                {
+                    string pass = Encoding.UTF8.GetString(sha.ComputeHash(Encoding.UTF8.GetBytes(maskedTextBox2.Text)));
+                    string npass = Encoding.UTF8.GetString(sha.ComputeHash(Encoding.UTF8.GetBytes(maskedTextBox3.Text)));
+                    if (pass == npass)
+                    {
+                        try
+                        {
+                            Registry.SetValue(Key, Value, pass, RegistryValueKind.String);//set the hash in the registry
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(this, ex.Message, "RPMPasswordSet", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+                else if (Encoding.UTF8.GetString(sha.ComputeHash(Encoding.UTF8.GetBytes(maskedTextBox1.Text))).Equals(Current))//the hashes are equals
+                {
+                    string pass = Encoding.UTF8.GetString(sha.ComputeHash(Encoding.UTF8.GetBytes(maskedTextBox2.Text)));
+                    string npass = Encoding.UTF8.GetString(sha.ComputeHash(Encoding.UTF8.GetBytes(maskedTextBox3.Text)));
+                    if (pass == npass)
+                    {
+                        try
+                        {
+                            Registry.SetValue(Key, Value, pass, RegistryValueKind.String);//set the hash in the registry
+                            MessageBox.Show(this, "Password Set!", "RPMPasswordSet", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            Current = pass;
+                            label1.Enabled = true;
+                            maskedTextBox1.Enabled = true;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(this, ex.Message, "RPMPasswordSet", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show(this, "The new passwords don't match", "RPMPasswordSet", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(this, "The old password is incorrect", "RPMPasswordSet", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "RPMPasswordSet", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LinkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/adryzz/RPMPasswordSet");
+        }
+    }
+}
